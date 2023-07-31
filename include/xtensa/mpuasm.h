f1f5e28ddc10f8efaa0c2091e55adb03d051503e
Michael Antioquia <michael.antioquia@intel.com>
2023-07-31 19:04:27 +0000

    User defined foreground map entries

--- include/xtensa/mpuasm.h
+++ include/xtensa/mpuasm.h
@@ -1,7 +1,7 @@
 /* $Id: //depot/dev/Foxhill/Xtensa/OS/include/xtensa/mpuasm.h#5 $ */
 
 /*
- * Copyright (c) 2016 Cadence Design Systems, Inc.
+ * Copyright (c) 2016-2022 Cadence Design Systems, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -37,12 +37,74 @@
  * 	a_temp3, a_temp4   
  */
 
-.macro mpu_write_map a_map, a_num_entries, a_temp1, a_temp2, a_temp3, a_temp4
+.macro mpu_write_map a_map, a_num_entries, a_temp1, a_temp2, a_temp3, a_temp4, a_temp5, a_temp6, a_temp7
 #if XCHAL_HAVE_MPU
 	movi	\a_temp1, 0
+#if XCHAL_HAVE_CACHEADRDIS
 	wsr.cacheadrdis \a_temp1 // enable the cache in all regions
+#endif
 	wsr.mpuenb	\a_temp1	// disable all foreground entries
+#if XCHAL_MPU_LOCK
+	// Write the specified map.  Any remaining mpu entries
+	// XCHAL_MPU_ENTRIES - a_num_entries
+	// are written with the last entry in the specified map
+	// (a_map[a_num_entries - 1]).
+	// These entries are initially written with the enable bit and lock bit
+	// cleared (the actual enable bits are stored in \a_temp1.
+	//
+	// Then all enable bits are set by writing the mpuenb register.
+	//
+	// Then the are written again to set the lock bits
 
+	// a_temp1 => enable bits
+	// a_temp2 => working index
+	// a_temp3 => pointer to working entry
+	// a_temp4 => at entry as read from memory  (with index bits cleared)
+	// a_temp5 => as entry as read frmom memory (with lock bit cleared)
+	// a_temp6 => scratch
+	// a_temp7 => scratch
+	movi    \a_temp2, 0
+	movi    \a_temp3, XCHAL_MPU_ENTRIES
+1:
+	bge     \a_temp2, \a_num_entries, 2f
+	addx8   \a_temp7, \a_temp2, \a_map
+.begin	no-schedule
+        l32i    \a_temp5, \a_temp7, 0        // get as (vstart, lock, enable)
+	l32i    \a_temp4, \a_temp7, 4        // get at (rights, memtype, entry)
+.end	no-schedule
+
+	extui   \a_temp6, \a_temp4, 0, 5     // entry index portion
+	xor     \a_temp4, \a_temp4, \a_temp6 // zero it
+2:
+	extui   \a_temp6, \a_temp5, 0, 2     // extract lock and enable bits
+	xor     \a_temp7, \a_temp5, \a_temp6 // zero lock and enable bits
+	or      \a_temp6, \a_temp4, \a_temp2 // or in entry field
+	wptlb   \a_temp6, \a_temp7           // write to MPU
+	slli    \a_temp7, \a_temp5, 31       // shift 'as' to put enable in MSB
+	srli    \a_temp1, \a_temp1, 1        // shift existing bit mask
+	or      \a_temp1, \a_temp1, \a_temp7 // or in the enable bit
+	addi    \a_temp2, \a_temp2, 1        // increment entry
+	bne     \a_temp2, \a_temp3, 1b       // continue throuh MPU
+	wsr.mpuenb \a_temp1
+        /* We need to do a final loop through the entries and rewrite the entries
+         * to get the lock bits set.  The original entry table and size is saved
+         * in a_temp6 and a_temp7 respectivly.
+         */
+	movi    \a_temp2, 0
+	mov     \a_temp3, \a_map
+3:
+.begin  no-schedule
+	l32i    \a_temp4, \a_temp3, 4      // get at (acc.rights, memtype)
+	l32i    \a_temp5, \a_temp3, 0      // get as (vstart, valid)
+.end    no-schedule
+        extui   \a_temp6, \a_temp4, 0, 5   // entry index portion
+        xor     \a_temp4, \a_temp4, \a_temp6    // zero it
+        or      \a_temp6, \a_temp4, \a_temp2    // or in entry field
+        wptlb   \a_temp6, \a_temp5         // write to MPU
+        addi    \a_temp2, \a_temp2, 1
+        addi    \a_temp3, \a_temp3, 8
+        bne     \a_temp2, \a_num_entries, 3b
+#else
     // Clear out the unused entries.
     //
     // Currently we are clearing out all the entries because currently
@@ -76,14 +138,17 @@
 	addi	\a_temp3, \a_temp3, -1
 	beqz	\a_num_entries, 4f		// loop until done
 3:	addi	\a_map, \a_map, -8
+	.begin	no-schedule
 	l32i	\a_temp2, \a_map, 4			// get at (acc.rights, memtype)
 	l32i	\a_temp4, \a_map, 0			// get as (vstart, valid)
+	.end	no-schedule
 	addi	\a_num_entries, \a_num_entries, -1
 	extui	\a_temp1, \a_temp2, 0, 5			// entry index portion
 	xor		\a_temp2, \a_temp2, \a_temp1			// zero it
 	or		\a_temp2, \a_temp2, \a_temp3			// set index = \a_temp3
 	j		2b
 4:
+#endif
 #endif
 .endm
 
